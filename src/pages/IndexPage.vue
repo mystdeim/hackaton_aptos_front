8cb1ccac1fdf58eba1c361edda371e7ce29d135e
Roman Novikov <mystdeim@gmail.com>
2023-09-27 12:35:30 +0100

    add list

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,29 @@
 <template>
-  <q-page class="flex">
-    <q-list dense>
-      <q-item v-for="(item, index) in items" :key="`row-${index}`" clickable v-ripple @click="show(item.id)" >
-        <q-item-section>{{ item.id.slice(0, 50) }}</q-item-section>
-      </q-item>
-    </q-list>
+  <q-page class="q-pa-lg">
+<!--    <q-list dense>-->
+<!--      <q-item v-for="(item, index) in items" :key="`row-${index}`" clickable v-ripple @click="show(item.id)" >-->
+<!--        <q-item-section>{{ item.id.slice(0, 50) }}</q-item-section>-->
+<!--      </q-item>-->
+<!--    </q-list>-->
+
+    <template v-for="(item, index) in items" :key="`row-${index}`" clickable v-ripple @click="show(item.id)" >-
+      <q-card class="my-card" dense>
+        <q-card-section horizontal>
+          <q-img
+            class="col-5"
+            src="https://cdn.quasar.dev/img/parallax1.jpg"
+          />
+          <q-card-section>
+<!--            {{ item.id.slice(0, 50) }}-->
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle4 word-wrap">{{ item.id }}</div>
+            <q-chip color="teal" text-color="white" icon="bookmark">
+              Bookmark = a
+            </q-chip>
+          </q-card-section>
+        </q-card-section>
+      </q-card>
+    </template>
 
     <q-dialog v-model="alert">
       <q-card>
@@ -24,6 +43,13 @@
 
   </q-page>
 </template>
+
+<style scoped>
+.word-wrap {
+  overflow-wrap: break-word;
+  word-break: break-all;
+}
+</style>
 
 <script setup>
 
